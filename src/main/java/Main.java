db7a01bd70f10e94379959c56326a9c46fd1fb33
Iker Preciado <ikerpreciado.g@gmail.com>
2026-08-09 21:58:17 -0600

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        // TODO hashmap with lambdas
         run(sc);
     }
 
@@ -10,7 +11,8 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String input = sc.nextLine();
-            System.out.println(input + ": command not found");  
+            if (input.equals("exit")) break;
+            System.out.println(input + ": command not found");
         }
 
     }
