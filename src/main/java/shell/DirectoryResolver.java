8155336de9c7066d8f5f7cd8344f320ebd179b08
Iker Preciado <ikerpreciado.g@gmail.com>
2026-08-22 23:03:01 -0600

    codecrafters submit [skip ci]

--- src/main/java/shell/DirectoryResolver.java
+++ src/main/java/shell/DirectoryResolver.java
@@ -3,7 +3,6 @@ package shell;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Optional;
 
 public class DirectoryResolver {
@@ -16,14 +15,29 @@ public class DirectoryResolver {
     public Optional<Path> findExecutable(String target) {
         Path targetPath = Path.of(target);
 
+        // Absolute
         if(targetPath.isAbsolute()) {
-            return Files.isExecutable(targetPath) ?
-                    Optional.of(targetPath) :
+            Path normalized = targetPath.normalize();
+
+            return Files.isExecutable(normalized) ?
+                    Optional.of(normalized) :
                     Optional.empty();
         }
 
+        // Local
+        Path currentWorkDir = ctx.getWorkingDir();
+        Path resolvedLocal = currentWorkDir.resolve(targetPath).normalize();
+
+        if (Files.isExecutable(resolvedLocal)) {
+            return Optional.of(resolvedLocal);
+        }
+
+        // Simple name
+        if (target.contains("/") || target.contains(File.separator))
+            return Optional.empty();
+
         for (String route : ctx.getPath().split(File.pathSeparator)) {
-            Path fullPath = Paths.get(route, target);
+            Path fullPath = Path.of(route).resolve(targetPath).normalize();
 
             if (Files.isExecutable(fullPath)) {
                 return Optional.of(fullPath);
@@ -35,19 +49,35 @@ public class DirectoryResolver {
     public Optional<Path> find(String target) {
         Path targetPath = Path.of(target);
 
+        // Absolute
         if (targetPath.isAbsolute()) {
-            return Files.exists(targetPath) ?
-                    Optional.of(targetPath) :
+            Path normalized = targetPath.normalize();
+
+            return Files.exists(normalized) ?
+                    Optional.of(normalized) :
                     Optional.empty();
         }
 
+        // Local
+        Path currentWorkDir = ctx.getWorkingDir();
+        Path resolvedLocal = currentWorkDir.resolve(targetPath).normalize();
+
+        if (Files.exists(resolvedLocal)) {
+            return Optional.of(resolvedLocal);
+        }
+
+        // Simple name
+        if (target.contains("/") || target.contains(File.separator))
+            return Optional.empty();
+
         for (String route : ctx.getPath().split(File.pathSeparator)) {
-            Path fullPath = Paths.get(route, target);
+            Path fullPath = Path.of(route).resolve(targetPath).normalize();
 
             if (Files.exists(fullPath)) {
                 return Optional.of(fullPath);
             }
         }
+
         return Optional.empty();
     }
 }
